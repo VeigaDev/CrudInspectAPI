c4e653d0c287b5ba7a818d86c1e09b5b9947faab
William Veiga <william.veiga@live.com>
2022-04-12 09:45:05 -0300

    Adicionar arquivos de projeto.

---
+++ InspectAPI/Inspection.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InspectAPI
+{
+    public class Inspection
+    {
+        public int Id { get; set; }
+        [StringLength(20)]
+        public string Status { get; set; } = String.Empty;
+        [StringLength(200)]
+        public string Coments { get; set; } = String.Empty;
+        public int InspectionTypeId { get; set; }
+        public InspectionType? InspectionType { get; set; }
+    }
+}
